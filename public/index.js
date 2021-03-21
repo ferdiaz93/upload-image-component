d1afda66372d645ac5c718287901a673d9d70dd2
ferdiaz93 <fernandodiaz1209@gmail.com>
2021-03-21 20:09:17 -0300

    first commit

---
+++ public/index.js
@@ -0,0 +1,49 @@
+const inputFile = document.getElementById("input-file");
+const form = document.getElementById("form");
+const imgElement = document.getElementById("img-preview");
+const inputName = document.getElementById("input-name");
+const responseText = document.getElementById("response-text");
+
+const user = {};
+
+inputFile.addEventListener("change", event => {
+    if (event.target.files && event.target.files[0]) {
+        let selfieFile = event.target.files[0];
+        //genera vista previa
+        let reader = new FileReader();
+        reader.onload = (e) => {
+            let photoPreview = e.target.result;
+            imgElement.src = photoPreview;
+        }
+        reader.readAsDataURL(selfieFile);
+        //guardamos el archivo dentro de nuestro user
+        user.file = selfieFile;
+    }
+});
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    user.username = inputName.value;
+    sendData(user);
+})
+
+
+function sendData(user){
+  let formData = new FormData();
+  formData.append('username', user.username);
+
+  //con este nombre vamos a poder capturarlo en nuestro server
+  formData.append('miarchivo', user.file);
+  fetch('http://localhost:8080/api/save-img', {
+    method: "POST",
+    body: formData
+  })
+  .then(res => res.json())
+  .then(response => {
+    if(response.success){
+      responseText.innerText = response.message + " ✅"
+    } else {
+      responseText.innerText = response.message + " ❌"
+    }
+  })
+}
---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const cors = require('cors')
+
+const app = express();
+app.use(cors());
+const upload = multer({ dest: 'server/temp'});
+const port = 8080;
+
+app.post('/api/save-img', upload.single('miarchivo'), (req, res) => {
+  let dateObj = new Date();
+  let month = dateObj.getUTCMonth() + 1;
+  let day = dateObj.getUTCDate();
+  let year = dateObj.getUTCFullYear();
+  const tempPath = req.file.path;
+  const extension = path.extname(req.file.originalname).toLowerCase();
+  const newFileName = `${year}_${month}_${day}_${Date.now()}`;
+  const targetPath = path.join(__dirname, `./uploads/${newFileName}${extension}`);
+  
+  if (extension === ".png" || extension === ".jpg") {
+    fs.rename(tempPath, targetPath, err => {
+      if (err) return res.json({error: err});
+      res.status(200).json({success: true, message: "Image uploaded"});
+    });
+  } else {
+    fs.unlink(tempPath, err => {
+      if (err) return res.json({error: err});
+      res.status(403).json({success: false, message: "Only .png or .jpg files are allowed!"});
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log("server iniciado en " + port);
+});
