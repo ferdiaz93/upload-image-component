643cb1ea6410576cd9af96cef34639e0b59c6cd3
ferdiaz93 <fernandodiaz1209@gmail.com>
2022-03-24 23:08:40 -0300

    actualizacion, nueva ruta para home

--- public/index.js
+++ public/index.js
@@ -1,29 +1,31 @@
-const inputFile = document.getElementById("input-file");
-const form = document.getElementById("form");
-const imgElement = document.getElementById("img-preview");
-const inputName = document.getElementById("input-name");
-const responseText = document.getElementById("response-text");
+const INPUT_FILE = document.getElementById("input-file");
+const FORM = document.getElementById("form");
+const IMG_ELEMENT = document.getElementById("img-preview");
+const INPUT_NAME = document.getElementById("input-name");
+const RESPONSE_TEXT = document.getElementById("response-text");
 
 const user = {};
 
-inputFile.addEventListener("change", event => {
+INPUT_FILE.addEventListener("change", event => {
     if (event.target.files && event.target.files[0]) {
         let selfieFile = event.target.files[0];
         //genera vista previa
+        //generate preview
         let reader = new FileReader();
         reader.onload = (e) => {
             let photoPreview = e.target.result;
-            imgElement.src = photoPreview;
+            IMG_ELEMENT.src = photoPreview;
         }
         reader.readAsDataURL(selfieFile);
         //guardamos el archivo dentro de nuestro user
+        //we save the file inside our user
         user.file = selfieFile;
     }
 });
 
-form.addEventListener("submit", event => {
+FORM.addEventListener("submit", event => {
     event.preventDefault();
-    user.username = inputName.value;
+    user.username = INPUT_NAME.value;
     sendData(user);
 })
 
@@ -33,17 +35,18 @@ function sendData(user){
   formData.append('filename', user.username);
 
   //con este nombre vamos a poder capturarlo en nuestro server
+  //with this name we can catch it from our server
   formData.append('miarchivo', user.file);
-  fetch('http://localhost:8080/api/save-img', {
+  fetch('/api/save-img', {
     method: "POST",
     body: formData
   })
   .then(res => res.json())
   .then(response => {
     if(response.success){
-      responseText.innerText = response.message + " ✅"
+      RESPONSE_TEXT.innerText = response.message + " ✅"
     } else {
-      responseText.innerText = response.message + " ❌"
+      RESPONSE_TEXT.innerText = response.message + " ❌"
     }
   })
 }
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
@@ -7,22 +8,34 @@ const cors = require('cors')
 const app = express();
 app.use(cors());
 const upload = multer({ dest: 'server/temp'});
-const port = 8080;
+const PORT = process.env.COMPONENT_PORT || 8080;
 
+//static middleware
+app.use(express.static(path.join(__dirname, '../public')))
+
+//index route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'../public/index.html'))
+})
+
+//upload.single is from multer lib
 app.post('/api/save-img', upload.single('miarchivo'), (req, res) => {
   let dateObj = new Date();
-  
+
   let month = dateObj.getUTCMonth() + 1;
   let day = dateObj.getUTCDate();
   let year = dateObj.getUTCFullYear();
 
   // el valor del primer input llega en req.body.filename
+  // the value of the first input arrives in
+  let fileNameParse = req.body.filename.replace(' ', '_');
 
   //este bloque de codigo crea un nombre en base al dia que se esta subiendo y ubicacion donde se va a guardar
-  const tempPath = req.file.path;
-  const extension = path.extname(req.file.originalname).toLowerCase();
-  const newFileName = `${year}_${month}_${day}_${Date.now()}`; 
-  const targetPath = path.join(__dirname, `./uploads/${newFileName}${extension}`);
+  //This code block creates a name based on the day it is being uploaded and the location where it will be saved.
+  let tempPath = req.file.path;
+  let extension = path.extname(req.file.originalname).toLowerCase();
+  let newFileName = `${fileNameParse}_${year}_${month}_${day}_${Date.now()}`; 
+  let targetPath = path.join(__dirname, `./uploads/${newFileName}${extension}`);
   
   if (extension === ".png" || extension === ".jpg") {
     fs.rename(tempPath, targetPath, err => {
@@ -37,6 +50,6 @@ app.post('/api/save-img', upload.single('miarchivo'), (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log("server iniciado en " + port);
+app.listen(PORT, () => {
+  console.log("SERVER IN PORT " + PORT);
 });
