0e3448f4ec9c29fc77de525e9206b53de317f823
Fernando Diaz <fernandodiaz1209@gmail.com>
2023-08-22 13:36:48 -0300

    index front actualizado

--- public/index.js
+++ public/index.js
@@ -7,6 +7,7 @@ const RESPONSE_TEXT = document.getElementById("response-text");
 const user = {};
 
 INPUT_FILE.addEventListener("change", event => {
+  RESPONSE_TEXT.innerText = "";
     if (event.target.files && event.target.files[0]) {
         let selfieFile = event.target.files[0];
         //genera vista previa
@@ -23,30 +24,37 @@ INPUT_FILE.addEventListener("change", event => {
     }
 });
 
-FORM.addEventListener("submit", event => {
+FORM.addEventListener("submit", async (event) => {
     event.preventDefault();
     user.username = INPUT_NAME.value;
-    sendData(user);
+    await sendData(user);
 })
 
 
-function sendData(user){
+async function sendData(user){
   let formData = new FormData();
   formData.append('filename', user.username);
 
   //con este nombre vamos a poder capturarlo en nuestro server
   //with this name we can catch it from our server
   formData.append('miarchivo', user.file);
-  fetch('/api/save-img', {
-    method: "POST",
-    body: formData
-  })
-  .then(res => res.json())
-  .then(response => {
-    if(response.success){
-      RESPONSE_TEXT.innerText = response.message + " ✅"
-    } else {
-      RESPONSE_TEXT.innerText = response.message + " ❌"
-    }
-  })
+  try {
+    const response = await fetch('/api/save-img', {
+      method: "POST",
+      body: formData
+    })
+    const data = await response.json();
+    showMessagge(data.success, data.message);
+  } catch (error) {
+    console.log(error);
+    showMessagge(false, error);
+  }
+}
+
+function showMessagge(success, message){
+  if(success){
+    RESPONSE_TEXT.innerText = message + " ✅"
+  } else {
+    RESPONSE_TEXT.innerText = message + " ❌"
+  }
 }
